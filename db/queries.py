13738d2b96e8100dac52c9700ebec7ca469b9f42
Kiran <Kiranpatil73827@gmail.com>
2026-02-20 19:28:18 +0530

    clean sql schemas and queries

--- db/queries.py
+++ db/queries.py
@@ -1,78 +1,25 @@
 from db.connect import get_cursor
 import uuid
 from datetime import datetime
-from models.models import AgentOutput,Plan,PlanStatus,Task,Request,Status,TaskType,TaskStatus
+from models.models import TaskType,TaskStatus
 import json
 con=get_cursor()
 
-def create_request(user_query:str):
+
+def now():
+    return datetime.utcnow()
+
+def create_task(**kwargs):
     cur=con.cursor()
-    current_time=datetime.now()
-    req_id = str(uuid.uuid4())
-    res=cur.execute(
-        """
-        INSERT INTO requests (id,user_query,status,plain_id,created_at,updated_at)
-        VALUES (?,?,?,?,?,?)
-        """,(req_id,user_query,Status.RECEIVED,current_time,current_time),
-        )
-    con.commit()
-
-    return res
-
-def update_requset_status(request_id: str,status: Status, plan_id: str | None = None):
-    cur=con.cursor()
-    cur.execute(
-    """
-    UPDATE requests
-    SET status=?, plan_id=COALESCE(?, plan_id), updated_at=?
-    WHERE  id=?
-
-    """,(Status.value,plan_id,request_id))
-    con.commit()
-
-
-def create_plan(request_id:str):
-    cur=con.cursor()
-    current_time=datetime.now()
-    plan_id=str(uuid.uuid4())
-    cur.execute(
-    """
-    INSERT INTO plans(id,request_id,status,created_at,updated_at)
-    VALUES (?,?,?,?,?)
-
-    """,(
-        plan_id, request_id,PlanStatus.ACTIVE.value,
-        current_time,current_time
-        )
-    )
-
-    con.commit()
-    return plan_id
-
-def plan_completed(plan_id:str):
-    cur=con.cursor()
-    current_time=datetime.now()
-
-    cur.execute(
-        """
-        UPDATE plans
-        SET status=?,completed_at=?,updated_at=?
-        WHERE id=?
-        """,(PlanStatus.COMPLETED.value,current_time,current_time,plan_id)
-    )
-    con.commit()
-
-
-def create_task(plan_id:str,task_type:TaskType,input_data:dict):
-    cur=con.cursor()
-    current_time=datetime.now()
 
     task_id=str(uuid.uuid4())
 
     cur.execute(
         """
-        INSERT INTO tasks(id, plan_id, type, status, input_json, created_at, updated_at)
-        """,(task_id,plan_id,task_type.value,TaskStatus.PENDING.value,json.dumps(input_data),current_time,current_time)
+        INSERT INTO tasks(id, plan_id, plan_step_id,parent_task_id, type, status,revision, input_json,retry_count,max_retries,next_run_at, created_at, updated_at)
+        VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)
+        """,
+        (task_id,kwargs["plan_id"],kwargs.get("parent_task_id"),kwargs["type"],TaskStatus.PENDING.value,kwargs.get("revision",0), json.dumps(kwargs["input_data"]),0,kwargs.get("max_retries",3), now(),now(),now())
     )
 
     con.commit()
@@ -82,12 +29,11 @@ def create_task(plan_id:str,task_type:TaskType,input_data:dict):
 
 def claim_task():
     cur=con.cursor()
-    current_time=datetime.now()
 
     task=cur.execute(
         """
-        SELECT * FROM tasks WHERE status=? ORDER BY created_at LIMIT 1
-        """,(TaskStatus.PENDING.value)
+        SELECT * FROM tasks WHERE status=? AND next_run_at<=? ORDER BY created_at LIMIT 1
+        """,(TaskStatus.PENDING.value,now())
     ).fetchone()
 
     if not task:
@@ -99,7 +45,7 @@ def claim_task():
         SET status=?, updated_at=?
         WHERE id=? AND status=?
 
-        """,(TaskStatus.IN_PROGRESS.value, current_time,task["id"], TaskStatus.PENDING.value)
+        """,(TaskStatus.IN_PROGRESS.value, now(),task["id"], TaskStatus.PENDING.value)
     )
 
     con.commit()
@@ -107,56 +53,48 @@ def claim_task():
     if update_task.rowcount==1:
         result=dict(task)
         return result
-    con.close()
     return None
 
 
 def finish_task(task_id:str):
     cur=con.cursor()
-    current_time=datetime.now()
 
     cur.execute(
         """
         UPDATE tasks
         SET status=?, updated_at=?
         WHERE id=?
-        """,(TaskStatus.FINISH.value, current_time,task_id)
+        """,(TaskStatus.FINISH.value, now(),task_id)
     )
     con.commit()
-    con.close()
 
 
-def agent_output(task_id:str,agent_type:str,payload:dict,status:str):
+def failed_task(task_id:str):
     cur=con.cursor()
-    id=str(uuid.uuid4())
-    current_time=datetime.now()
 
     cur.execute(
         """
-        INSERT INTO agent_outputs(id, task_id, agent_type, status, payload_json,created_at)
-        VALUES(?,?,?,?,?,?)
+        UPDATE tasks SET status='FAILED', updated_at=?
+        WHERE id=?
 
-        """,(id,task_id,agent_type,status,json.dumps(payload),current_time)
+        """,(now(),task_id)
     )
+
     con.commit()
-    con.close()
 
+def update_task_retry(task_id,next_run_at):
+    cur=con.cursor()
 
+    cur.execute(
+        """
+        UPDATE tasks
+        SET retry_count=retry_count+1,
+            status='PENDING',
+            next_run_at=?,
+            updated_at=? WHERE id=?
 
-def _row_to_plan(row):
-    return Plan(
-        id      = row["id"],
-        request_id= row["request_id"],
-        status= PlanStatus(row["status"]),
-        user_query= row["user_query"],
-        max_revision=  row["max_revisions"],
-        current_revision=row["current_revision"],
-        version        = row["version"],
-        # created_at     = _req_dt(row["created_at"]),
-        # updated_at     = _req_dt(row["updated_at"]),
-        # completed_at   = _dt(row["completed_at"]),
+        """,(next_run_at,now(),task_id)
     )
 
-
-
+    con.commit()
 
--- db/schema.sql
+++ db/schema.sql
@@ -11,11 +11,11 @@ CREATE TABLE IF NOT EXISTS user_requests(
 
 CREATE TABLE IF NOT EXISTS plans (
     id    TEXT PRIMARY KEY,
-    request_id    TEXT NOT NULL REFERENCES requests(id),
+    request_id    TEXT NOT NULL REFERENCES user_requests(id),
     status      TEXT NOT NULL DEFAULT 'ACTIVE',
-    user_query   TEXT NOT NULL,
-    max_revisions   INTEGER NOT NULL DEFAULT 3,
-    revision_count  INTEGER NOT NULL DEFAULT 0,
+    -- user_query   TEXT NOT NULL,
+    -- max_revisions   INTEGER NOT NULL DEFAULT 3,
+    -- revision_count  INTEGER NOT NULL DEFAULT 0,
     created_at    TEXT NOT NULL,
     updated_at    TEXT NOT NULL,
     completed_at  TEXT        
@@ -27,22 +27,23 @@ CREATE INDEX IF NOT EXISTS idx_plans_status     ON plans(status);
 
 CREATE TABLE IF NOT EXISTS tasks (
     id      TEXT PRIMARY KEY,
-    plan_id    TEXT NOT NULL REFERENCES plans(id),
+    plan_id TEXT NOT NULL REFERENCES plans(id),
+    -- plan_step_id    TEXT NOT NULL REFERENCES plan_steps(id),
+    parent_task_id TEXT,
     type         TEXT NOT NULL,
     status      TEXT NOT NULL DEFAULT 'PENDING',
     -- priority      INTEGER NOT NULL DEFAULT 5,
-    input_json      TEXT NOT NULL DEFAULT '{}',  
-    assigned_agent  TEXT,                          
+    revision INTEGER DEFAULT 0,
+    input_json      TEXT NOT NULL DEFAULT '{}',                         
     retry_count     INTEGER NOT NULL DEFAULT 0,
     max_retries     INTEGER NOT NULL DEFAULT 3,
+    next_run_at  TEXT,
     -- version         INTEGER NOT NULL DEFAULT 1,      
     created_at   TEXT NOT NULL,
     updated_at   TEXT NOT NULL
 );
 
-CREATE INDEX IF NOT EXISTS idx_tasks_plan_id ON tasks(plan_id);
 CREATE INDEX IF NOT EXISTS idx_tasks_status  ON tasks(status);
-CREATE INDEX IF NOT EXISTS idx_tasks_type    ON tasks(type);
 
 CREATE TABLE IF NOT EXISTS agent_outputs (
     id       TEXT PRIMARY KEY,
