05c2a80726444da50ef7abfbd8b60164337e7419
Kiran <Kiranpatil73827@gmail.com>
2026-02-21 14:40:37 +0530

    Init react+tailwindcss frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div>
+      <h2 className='bg-red-400'>HEllo</h2>
+    </div>
+    </>
+  )
+}
+
+export default App
